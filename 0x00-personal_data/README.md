2e555530e801b8efae435d21af1f87b4434a2f16
uthmanduro <udurosinlohun@gmail.com>
2023-08-04 03:58:08 +0100

    task 1 and 2

---
+++ 0x00-personal_data/README.md
@@ -0,0 +1,1 @@
+learning how to use the logging module
---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""learning python logs"""
+
+
+import re
+import logging
+
+
+def filter_datum(fields, redaction, message, separator):
+    """returns the log message obfuscated"""
+    for field in fields:
+        message = re.sub(field + "=.*?" + separator,
+                         field + "=" + redaction + separator, message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: list):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum."""
+        return filter_datum(self.fields, self.REDACTION,
+                            super(RedactingFormatter, self).format(record),
+                            self.SEPARATOR)
