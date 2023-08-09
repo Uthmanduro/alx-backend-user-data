1b7955859024ba41c5af1163ebbe5bb00879398e
uthmanduro <udurosinlohun@gmail.com>
2023-08-09 16:37:35 +0100

    implemented the before_request handler

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""module to manage the API authentication"""
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class to manage the authentication API"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """returns true if path is not in excluded_paths"""
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+        if path in excluded_paths:
+            return False
+        if path[-1] != '/':
+            path += '/'
+            if path in excluded_paths:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """method that handles authorization header"""
+        if request is None:
+            return None
+        if 'Authorization' not in request.headers:
+            return None
+        return request.headers.get('Authorization')
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        """method that handles current user"""
+        return None
