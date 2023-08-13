87d9b799de453dcefb7a68363f25e2ece03b6088
Uthman Durosinlohun <udurosinlohun@gmail.com>
2023-08-13 16:59:43 -0700

    refacetored app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -19,6 +19,19 @@ if AUTH_TYPE == 'auth':
     auth = Auth()
 
 
+@app.before_request
+def auth_before_request():
+    """filter each request"""
+    if auth is None:
+        return
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/']
+    if auth.require_auth(request.path, excluded_paths):
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
+
 
 @app.errorhandler(404)
 def not_found(error) -> str:
@@ -32,21 +45,6 @@ def unauthorized(error) -> str:
     """ Unauthorized handler
     """
     return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.before_request
-def auth_before_request():
-    """filter each request"""
-    if auth is None:
-        return
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
-    if auth.require_auth(request.path, excluded_paths):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
 
 
 @app.errorhandler(403)
